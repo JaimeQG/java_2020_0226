7894c5e0f3788f0b8b7bf0d8bb794386054c99df
JaimeQG <jquinta0@hotmail.com>
2021-01-21 23:57:51 +0100

    Formulario de Producto

---
+++ springmvcbasico/src/main/java/com/ipartek/formacion/spring/springmvcbasico/controladores/ProductoController.java
@@ -0,0 +1,26 @@
+package com.ipartek.formacion.spring.springmvcbasico.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ipartek.formacion.spring.springmvcbasico.entidades.Producto;
+
+import lombok.extern.java.Log;
+
+@Log
+@Controller
+@RequestMapping("/producto")
+public class ProductoController {
+	@GetMapping
+	public String getProducto() {
+		return "producto"; // Nombre de la vista/template
+	}
+
+	@PostMapping
+	public String postProducto(Producto producto) {
+		log.info(producto.toString());
+		return "producto";
+	}
+}
