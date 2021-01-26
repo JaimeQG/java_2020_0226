f45dc24d6849de5db8aacc1ad0da029ca4dc922d
JaimeQG <jquinta0@hotmail.com>
2021-01-26 23:20:03 +0100

    Ejemplo JPA / Hibernate

---
+++ ejemplojpa/src/main/java/com/ipartek/formacion/jpa/ejemplojpa/App.java
@@ -0,0 +1,35 @@
+package com.ipartek.formacion.jpa.ejemplojpa;
+
+import java.time.LocalDate;
+
+import com.ipartek.formacion.jpa.ejemplojpa.accesodatos.AccesoDatosException;
+import com.ipartek.formacion.jpa.ejemplojpa.accesodatos.ClienteJpaDao;
+import com.ipartek.formacion.jpa.ejemplojpa.accesodatos.Dao;
+import com.ipartek.formacion.jpa.ejemplojpa.entidades.Cliente;
+
+public class App {
+	public static void main(String[] args) {
+		try {
+			Dao<Cliente> dao = new ClienteJpaDao();
+
+			for (int i = 1; i < 10; i++) {
+				dao.agregar(new Cliente(null, "Nombre" + i, "Apellidos" + i, "0000000" + i + (char) ('A' + i),
+						LocalDate.now()));
+			}
+
+			dao.borrar(7L);
+
+			System.out.println(dao.obtenerPorId(1L));
+
+			dao.modificar(new Cliente(2L, "Juan", "Juanes", "87654321A", LocalDate.now()));
+
+			for (Cliente cliente : dao.obtenerTodos()) {
+				System.out.println(cliente);
+			}
+
+			System.out.println("Numero de registros en la BBDD: " + dao.numeroRegistros());
+		} catch (AccesoDatosException e) {
+			System.out.println("Error de acceso a datos:" + e.getMessage());
+		}
+	}
+}
---
+++ ejemplojpa/src/main/java/com/ipartek/formacion/jpa/ejemplojpa/accesodatos/Dao.java
@@ -0,0 +1,15 @@
+package com.ipartek.formacion.jpa.ejemplojpa.accesodatos;
+
+public interface Dao<T> {
+	Iterable<T> obtenerTodos();
+
+	T obtenerPorId(Long id);
+
+	T agregar(T objeto);
+
+	T modificar(T objeto);
+
+	void borrar(Long id);
+
+	int numeroRegistros();
+}
