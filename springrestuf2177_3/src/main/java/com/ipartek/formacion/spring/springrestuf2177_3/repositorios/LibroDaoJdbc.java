2c86171312506de25c9c4f6fbc440a829f9aacc9
JaimeQG <jquinta0@hotmail.com>
2021-02-12 08:11:15 +0100

    UF2177_3

--- springrestuf2177_3/src/main/java/com/ipartek/formacion/spring/springrestuf2177_3/repositorios/LibroDaoJdbc.java
+++ springrestuf2177_3/src/main/java/com/ipartek/formacion/spring/springrestuf2177_3/repositorios/LibroDaoJdbc.java
@@ -3,6 +3,7 @@ package com.ipartek.formacion.spring.springrestuf2177_3.repositorios;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -28,18 +29,30 @@ public class LibroDaoJdbc implements Dao<Libro> {
 	@Override
 	public Libro buscarPorId(int id) {
 
-		return jdbc.queryForObject(
-				"SELECT l.id, l.nombre, CONCAT(a.nombre,' ',a.apellidos) \r\n" + "FROM libros as l \r\n"
-						+ "JOIN autores AS a ON a.id = l.autores_id \r\n" + "WHERE l.id = ?",
-				new BeanPropertyRowMapper<Libro>(Libro.class), id);
+		try {
+			return jdbc.queryForObject(
+					"SELECT l.id, l.nombre, CONCAT(a.nombre,' ',a.apellidos) \r\n" + "FROM libros as l \r\n"
+							+ "JOIN autores AS a ON a.id = l.autores_id \r\n" + "WHERE l.id = ?",
+					new BeanPropertyRowMapper<Libro>(Libro.class), id);
+		} catch (DataAccessException e) {
+			return null;
+		}
 	}
 
 	public Libro buscarPorIdConResenias(int id) {
 		Libro libro = buscarPorId(id);
 
+		if (libro == null) {
+			return null;
+		}
+
 		List<Resenia> resenias = jdbc.query(
 				"SELECT r.id, r.texto, r.libros_id \r\n" + "FROM resenas r WHERE r.libros_id=? ORDER BY r.id ASC",
 				(rs, rowNum) -> new Resenia(rs.getInt("id"), rs.getString("texto"), rs.getInt("r.libros_id")), id);
+
+		for (Resenia resenia : resenias) {
+			System.out.println(resenia);
+		}
 
 		libro.getResenias().addAll(resenias);
 
