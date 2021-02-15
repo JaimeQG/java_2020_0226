6a99a12ff1d7ce5e460e404c1927109032e268a7
JaimeQG <jquinta0@hotmail.com>
2021-02-15 17:00:10 +0100

    Servicio REST con Spring MVC, Spring JDBC Template y Spring Data JPA

---
+++ servidorrestspring/src/main/java/com/ipartek/formacion/spring/servidorrestspring/repositorios/ClienteRepository.java
@@ -0,0 +1,8 @@
+package com.ipartek.formacion.spring.servidorrestspring.repositorios;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ipartek.formacion.spring.servidorrestspring.entidades.Cliente;
+
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+}
